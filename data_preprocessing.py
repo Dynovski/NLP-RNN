808c9d5762c85a79085d71cdca12c373358cc703
Zbigniew Dynowski <dynowski@basement.pl>
2021-12-16 14:08:18 +0100

    Embedding untested

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import re
 import string
+import numpy as np
 
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from plotly import graph_objs as go
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+from nltk.tokenize import word_tokenize
 from sklearn.preprocessing import LabelEncoder
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 from exceptions import IncorrectNumberOfAttributesException
 
@@ -33,6 +37,8 @@ class DataPreProcessor:
             Stopwords for english for preprocessing
         stemmer: nltk.stem.SnowballStemer
             Stemmer for english used for preprocessing
+        tokenizer: keras.preprocessing.text.Tokenizer
+            Tokenizer used to process text data to indices
     """
     def __init__(
             self,
@@ -56,7 +62,8 @@ class DataPreProcessor:
             self.data['data_len'] = self.data[calculate_len[1]].apply(lambda x: len(x.split(' ')))
 
         self.stopwords: List[str] = stopwords.words('english')
-        self.stemmer = SnowballStemmer('english')
+        self.stemmer: SnowballStemmer = SnowballStemmer('english')
+        self.tokenizer: Tokenizer = Tokenizer()
 
     def max_element_length(self) -> int:
         """
@@ -162,9 +169,9 @@ class DataPreProcessor:
             Name of the attribute containing labels for data
         :return: None
         """
-        encoder = LabelEncoder()
+        encoder: LabelEncoder = LabelEncoder()
         encoder.fit(self.data[attribute])
-        self.data[f'encoded_{attribute}'] = encoder.transform(self.data[attribute])
+        self.data[f'encoded_labels'] = encoder.transform(self.data[attribute])
 
     def get_data(self) -> pd.DataFrame:
         """
@@ -177,7 +184,7 @@ class DataPreProcessor:
 
     def preprocess(self, data_attr: str, label_attr: str) -> pd.DataFrame:
         """
-        Invoke this function to implicitly apply all possible processing to the data
+        Call this function to implicitly apply all possible processing to the data
 
         :param data_attr: str
             name of an attribute containing data to process
@@ -191,3 +198,67 @@ class DataPreProcessor:
         self.encode_labels(label_attr)
 
         return self.data
+
+    @property
+    def preprocessed_data(self):
+        return self.data['clean_data']
+
+    @property
+    def encoded_labels(self):
+        return self.data['encoded_labels']
+
+    def tokenize(self) -> np.ndarray:
+        """
+        Tokenize preprocessed data, convert it to numerical values and pad it with zeroes at the end
+
+        :return: numpy.ndarray
+             Array of numerical values where each row corresponds to the same row of preprocessed data
+        """
+        self.tokenizer.fit_on_texts(self.preprocessed_data)
+
+        longest_sequence: str = max(self.preprocessed_data, key=lambda t: len(word_tokenize(t)))
+        longest_sentence_len: int = len(word_tokenize(longest_sequence))
+
+        padded_sequences: np.ndarray = pad_sequences(
+            self.tokenizer.texts_to_sequences(self.preprocessed_data),
+            longest_sentence_len,
+            padding='post'
+        )
+
+        return padded_sequences
+
+    def create_glove_embedding_matrix(self, path: str, embedding_dim: int, vocab_len: int) -> np.ndarray:
+        """
+        Creates glove embedding matrix
+
+        :param path: str
+            Path to locally stored file containing pretrained glove model
+        :param embedding_dim: int
+            Dimension of glove vector
+        :param vocab_len: int
+            Number of unique tokens
+        :return: numpy.ndarray
+            Matrix containing vector representation for each consecutive word in vocabulary
+        """
+        word_to_vector_d: Dict[str, np.ndarray] = dict()
+
+        with open(path) as f:
+            for line in f.readlines():
+                elements = line.split()
+                word = elements[0]
+                vector = np.asarray(elements[1:], dtype='float32')
+                word_to_vector_d[word] = vector
+
+        embedding_matrix = np.zeros((vocab_len, embedding_dim))
+
+        count: int = 0
+        for word, index in self.tokenizer.word_index.items():
+            vector = word_to_vector_d.get(word)
+            if vector is not None:
+                count += 1
+                embedding_matrix[index] = vector
+
+        print(f'Glove has {count} of {len(self.tokenizer.word_index) + 1} words in vocabulary')
+
+        return embedding_matrix
+
