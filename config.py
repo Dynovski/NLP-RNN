57f2e12660544c81a87332cf54657928ef2dcf1f
Zbigniew Dynowski <zbigniewdynowski@gmail.com>
2021-12-29 15:48:32 +0100

    new plots

--- config.py
+++ config.py
@@ -17,21 +17,21 @@ class NetworkType(Enum):
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 CHECKPOINTS_FOLDER = 'checkpoints'
-CHECKPOINT_NAME = 'checkpoint_tweet_lstm.pth'
-LOSS_PLOT_NAME = 'tweet_lstm'
-CM_NAME = 'tweet_lstm_cm'
-CM_LABELS = ['Real', 'Disaster']
+CHECKPOINT_NAME = 'checkpoint_news_lstm.pth'
+LOSS_PLOT_NAME = 'news_lstm'
+CM_NAME = 'news_lstm_cm'
+CM_LABELS = ['World', 'Sports', 'Business', 'Sci/Tech']
 
-TASK_TYPE = TaskType.TWEET
+TASK_TYPE = TaskType.NEWS
 NETWORK_TYPE = NetworkType.LSTM
-IS_MULTICLASS = False
-TRAIN_DATA_RATIO = 0.8
+IS_MULTICLASS = True
+TRAIN_DATA_RATIO = 0.9
 BATCH_SIZE = 32
 HIDDEN_STATE_SIZE = 256
 EMBEDDING_VECTOR_SIZE = 100
 NUM_RECURRENT_LAYERS = 2
-OUTPUT_CLASSES = 1
-VALIDATE_EVERY = 20
+OUTPUT_CLASSES = 4
+VALIDATE_EVERY = 500
 CLIP_VALUE = 0.1
 
 EPOCHS = 5
